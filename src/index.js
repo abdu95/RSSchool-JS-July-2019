895968432063d9d837692ee5bd68f9db5554cb1a
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 00:23:46 +0500

    fourth attempt

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+    if (preferences.length == 1)
+        return 1;
+};
